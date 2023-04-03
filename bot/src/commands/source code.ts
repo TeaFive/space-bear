a9fd579dfa68a1910ed4de56075425b9c6fa9e47
ReallPanda <douglashansenjr@gmail.com>
2023-04-03 16:35:25 -0700

    more changes

--- bot/src/commands/source code.ts
+++ bot/src/commands/source code.ts
@@ -9,18 +9,16 @@ import { Discord, Slash } from 'discordx';
 
 @Discord()
 export class Pat {
-  @Slash({ name: 'leaderboard', description: 'Get the servers leaderboard' })
+  @Slash({ name: 'source-code', description: 'Get the servers leaderboard' })
   async send(interaction: CommandInteraction): Promise<void> {
     if (!interaction.guild) return;
 
     const row =
       new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
         new ButtonBuilder()
-          .setLabel(`${interaction.guild.name}'s Leaderboard`)
+          .setLabel(`Space Bears Source Code!`)
           .setStyle(ButtonStyle.Link)
-          .setURL(
-            `https://space-bear.vercel.app//leaderboard/${interaction.guild.id}`
-          )
+          .setURL(`https://github.com/FroggyPanda/space-bear`)
       );
 
     interaction.reply({ content: 'Here you go!', components: [row] });
